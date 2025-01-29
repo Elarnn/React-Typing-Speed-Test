739c33aaa7cfe2043fc76810925ebcf7ae3e1674
Elarnn <sashafadyeyev@gmail.com>
2025-01-29 16:21:09 +0200

    Added new text cursor

--- src/components/TextDisplay.js
+++ src/components/TextDisplay.js
@@ -1,11 +1,15 @@
-import "../css/TextDisplay.css"; // Импортируем стили
+import "../css/TextDisplay.css";
 
 const TextDisplay = ({ targetText, position, isError }) => {
+    // Если текущий символ - пробел или конец строки, заменяем его на неразрывный пробел
+    const currentChar = targetText[position] === " " || position >= targetText.length ? "\u00A0" : targetText[position];
+
     return (
         <p className="text-display">
             <span className="text-correct">{targetText.slice(0, position)}</span>
             <span className={`text-current ${isError ? "text-error" : ""}`}>
-                {targetText[position] || " "}
+                {currentChar}
+                <span className="text-cursor"></span> {/* Курсор внутри text-current */}
             </span>
             <span className="text-remaining">{targetText.slice(position + 1)}</span>
         </p>
